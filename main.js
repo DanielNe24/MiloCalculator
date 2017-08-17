d4ef29b8c00e987105c5512a7ca4417a91cf9912
System Administrator <root@Daniels-MacBook-Pro.local>
2017-08-17 16:46:04 +0300

    first commi

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 var express = require('express');        
 var app = express();         
 var bodyParser = require('body-parser');
+var ipgeoblock = require("node-ipgeoblock");
 var cors = require('cors');
 var session = require('express-session')
 var port = process.env.PORT || 8080;       
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
+
+app.use(ipgeoblock({
+    geolite2: "./GeoLite2-Country.mmdb",
+    allowedCountries: [ "IL" ]
+}));
 
 // Add headers
 app.use(function (req, res, next) {
