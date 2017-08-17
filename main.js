1dd5504d120453c37d4a741f39e2a6633058e56c
System Administrator <root@Daniels-MacBook-Pro.local>
2017-08-17 16:48:44 +0300

    first commi

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 
 app.use(ipgeoblock({
     geolite2: "./GeoLite2-Country.mmdb",
-    allowedCountries: [ "IL" ]
+    allowedCountries: [ "IL" , "US"]
 }));
 
 // Add headers
