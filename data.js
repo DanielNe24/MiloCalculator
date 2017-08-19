b395cc979ebfe9fa2d87c5788564fe26e18810b3
System Administrator <root@Daniels-MacBook-Pro.local>
2017-08-19 13:48:35 +0300

    first commi

--- data.js
+++ data.js
@@ -156,4 +156,31 @@ DIL200910HS.priority = 4;
 Products = [];
 Products.push(LHV300,LHV190,E100,E150,DIL100130,CB250PLUS,CB100,CB100PLUS,B15_B15PLUS,B10_B10PLUS,DIL100270LS,DIL100270HS,DIL150560LS,DIL150560HS,DIL200910LS,DIL200910HS);
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+	results();
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 }
